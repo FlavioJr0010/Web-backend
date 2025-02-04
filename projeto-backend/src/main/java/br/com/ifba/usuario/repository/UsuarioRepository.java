01c2e8ebd152e3bb64a25fed122418b97107cc12
FlavioJr0010 <juniornordeste10@hotmail.com>
2025-02-04 19:20:31 -0300

    Correção da misera da atividade

--- projeto-backend/src/main/java/br/com/ifba/usuario/repository/UsuarioRepository.java
+++ projeto-backend/src/main/java/br/com/ifba/usuario/repository/UsuarioRepository.java
@@ -4,12 +4,17 @@ import br.com.ifba.usuario.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 // Define este repositório como um componente gerenciado pelo Spring
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
     // Método abstrato para deletar um usuário pelo ID
     // Não é necessário declará-lo, pois JpaRepository já fornece esse método
-    @Override
     void deleteById(Long id);
+
+    Optional<Usuario> findByLogin(String login);
+
+    Optional<Usuario> findByLoginAndSenha(String login, String senha);
 }
